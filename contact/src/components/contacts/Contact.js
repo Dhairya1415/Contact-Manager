f9e2708f213acc35a1eeec60547e1a8ebfa131be
Your Name <you@example.com>
2018-12-24 15:26:46 +0530

    Contact Manager

---
+++ contact/src/components/contacts/Contact.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { Consumer } from "../../context";
+import Axios from "axios";
+
+class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  static propTypes = {
+    contact: PropTypes.object.isRequired
+  };
+
+  onDeleteClick = async (id, dispatch) => {
+    try {
+      await Axios.get(`http://jsonplaceholder.typicode.com/users/${id}`);
+      dispatch({ type: "DELETE_CONTACT", payload: id });
+    } catch (e) {
+      dispatch({ type: "DELETE_CONTACT", payload: id });
+    }
+  };
+
+  render() {
+    const { name, email, phone, id } = this.props.contact;
+    const { showContactInfo } = this.state;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name}{" "}
+                <i
+                  className="fa fa-sort-down"
+                  onClick={() =>
+                    this.setState({
+                      showContactInfo: !this.state.showContactInfo
+                    })
+                  }
+                  style={{ cursor: "pointer" }}
+                />
+                <i
+                  className="fa fa-times"
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                  style={{ cursor: "pointer", color: "red", float: "right" }}
+                />
+                <Link to={`/edit/${id}`}>
+                  <i
+                    className="fas fa-pencil-alt"
+                    style={{
+                      cursor: "pointer",
+                      float: "right",
+                      color: "black",
+                      marginRight: "1rem"
+                    }}
+                  />
+                </Link>
+              </h4>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">{phone}</li>
+                  <li className="list-group-item">{email}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+// Contact.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   email: PropTypes.string.isRequired,
+//   phone: PropTypes.string.isRequired
+// };
+export default Contact;
---
+++ contact/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="display-4">About Contact Manager</h1>{" "}
+      {/*props.match.params.id*/}
+      <p className="lead">Simple App to manage Contacts</p>
+      <p className="text-secondary">Version 1.0.0</p>
+    </div>
+  );
+}
