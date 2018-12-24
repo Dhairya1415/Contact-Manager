8892ef117926403acdbcbfa8cc03fb77dfee1152
Your Name <you@example.com>
2018-12-24 17:59:57 +0530

    Updates

---
+++ precache-manifest.8d8581820dded7eac1df4b98a974a09d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65dd574dc71532402377",
+    "url": "/Contact-Manager/static/js/runtime~main.65dd574d.js"
+  },
+  {
+    "revision": "4e802b4de7e54e68ec67",
+    "url": "/Contact-Manager/static/js/main.4e802b4d.chunk.js"
+  },
+  {
+    "revision": "86cc053d07e8a3c871a0",
+    "url": "/Contact-Manager/static/js/1.86cc053d.chunk.js"
+  },
+  {
+    "revision": "4e802b4de7e54e68ec67",
+    "url": "/Contact-Manager/static/css/main.0a20e16e.chunk.css"
+  },
+  {
+    "revision": "86cc053d07e8a3c871a0",
+    "url": "/Contact-Manager/static/css/1.dabde468.chunk.css"
+  },
+  {
+    "revision": "4097135ea92b361b3eb5eaa541beb32b",
+    "url": "/Contact-Manager/index.html"
+  }
+];
